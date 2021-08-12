2d29c00dd806599304423da35febed4037b0da73
BearingMe <gomes.bruno201@gmail.com>
2021-08-12 10:57:07 -0300

    [FEAT][FIX] remove componentes e adiciona novos
    foi removido os componentes de exemplo para facilitar o testes de
    novos, a adicionado o de about

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const Bot = require("./core");
 
-const exampleA = require("./components/ordinary/exampleA");
-const exampleB = require("./components/ordinary/exampleB");
-const exampleC = require("./components/ordinary/exampleC");
-const exampleD = require("./components/ordinary/exampleD");
+const about = require("./components/ordinary/about");
+// const exampleA = require("./components/ordinary/exampleA");
+// const exampleB = require("./components/ordinary/exampleB");
+// const exampleC = require("./components/ordinary/exampleC");
+// const exampleD = require("./components/ordinary/exampleD");
 
 let bot = new Bot({
-  components: [exampleA, exampleB, exampleC, exampleD],
+  components: [about],
 });
 
 bot.start();
