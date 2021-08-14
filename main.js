20a11f0e9e9930a8f1b2bf0deec7420c3719e09e
BearingMe <gomes.bruno201@gmail.com>
2021-08-14 09:39:05 -0300

    [REFACTOR] registra novos componentes

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const Bot = require("./core");
 
-const about = require("./components/default_about");
+// on message
+const autoban = require('./components/message/admin_autoban') // funcionando | em desenvolvimento
+
+// on message create
+const about = require("./components/message_create/default_about"); // completo
+const counter = require('./components/message_create/default_counter') // completo
 
 let bot = new Bot({
-  components: [about],
+  components: [about, autoban, counter],
 });
 
 bot.start();
