7a8320c5ed6a2ac6bd4ec3fcb046229b0274de0b
BearingMe <gomes.bruno201@gmail.com>
2021-10-14 15:11:28 -0300

    [FEAT] importa os novos componentes
    foi adicionado um import/export para cada componente. a partir daqui
    todos serão registrados

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import { about } from "./_about";
+import { count } from "./_count";
+import { creator } from "./_creator";
+import { flip } from "./_flip";
+import { forbidden_link } from "./_forbidden_link";
+import { links } from "./_links";
+import { jokenpo } from "./_jokenpo";
+
+export const metadata = {
+  about,
+  count,
+  creator,
+  flip,
+  forbidden_link,
+  links,
+  jokenpo,
+};
