51d974333cd54afe51ad4cccd3b3039d541ebcda
BearingMe <gomes.bruno201@gmail.com>
2021-10-14 15:04:31 -0300

    [FEAT] adiciona componente flip
    joga uma moeda com o resultado cara ou coroa

---
+++ src/components/_flip/index.ts
@@ -0,0 +1,14 @@
+import { MetadataProtocol } from "../interfaces/metadata_protocol";
+
+function template(msg: any): void {
+  let number = Math.round(Math.random() * 1)
+  let choice = ['cara', 'coroa'][number]
+
+  msg.reply(choice);
+}
+
+export const flip: MetadataProtocol = {
+  alias: /^!flip$/,
+  trigger: "message_create",
+  template: template,
+};
