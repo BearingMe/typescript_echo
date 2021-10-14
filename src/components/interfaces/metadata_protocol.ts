28930580d61745c5b58427b073b48c8dd326544c
BearingMe <gomes.bruno201@gmail.com>
2021-10-14 18:38:55 -0300

    [FEAT] cria uma interface para os metadados

--- src/components/interfaces/metadata_protocol.ts
+++ src/components/interfaces/metadata_protocol.ts
@@ -1,7 +1,9 @@
 type TriggerTypes = "message" | "message_create"
+type ScopeTypes = "anywhere" | "group_only" | "private_only"
 
 export interface MetadataProtocol {
   alias: RegExp;
   trigger: TriggerTypes;
+  scope?: ScopeTypes,
   template(msg: any): void
 }
