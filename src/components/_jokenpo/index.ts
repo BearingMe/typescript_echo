198254114200c3f2c9e352a7ce85fec2ad7d877d
BearingMe <gomes.bruno201@gmail.com>
2021-10-14 15:09:05 -0300

    [FEAT] adiciona o componente jokenpo
    retorna pedra, papel ou tesoura

---
+++ src/components/_jokenpo/index.ts
@@ -0,0 +1,14 @@
+import { MetadataProtocol } from "../interfaces/metadata_protocol";
+
+function template(msg: any): void {
+  let number = Math.round(Math.random() * 2) 
+  let choice = ['pedra', 'papel', 'tesoura'][number]
+
+  msg.reply(choice);
+}
+
+export const jokenpo: MetadataProtocol = {
+  alias: /^!jokenpo$/,
+  trigger: "message_create",
+  template: template,
+};
