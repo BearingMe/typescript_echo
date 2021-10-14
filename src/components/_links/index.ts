ae3e782d891ca5a3524301039266734ccf9eb747
BearingMe <gomes.bruno201@gmail.com>
2021-10-14 15:05:10 -0300

    [FEAT] adiciona o componente links
    mostra links uteis

---
+++ src/components/_links/index.ts
@@ -0,0 +1,14 @@
+import { MetadataProtocol } from "../interfaces/metadata_protocol";
+import { openTXT } from "../utils/file";
+
+const linksText: string = openTXT(__dirname, "links.txt");
+
+function template(msg: any): void {
+  msg.reply(linksText);
+}
+
+export const links: MetadataProtocol = {
+  alias: /^!links$/,
+  trigger: "message_create",
+  template: template,
+};
