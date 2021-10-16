8486a7052670b6dd26b4058f22fdb77b4eb95469
BearingMe <gomes.bruno201@gmail.com>
2021-10-16 00:01:42 -0300

    [REFACTOR] atualiza a chamada dos componentes
    aplica a interface de filtros assincronos e os novos filtros

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { Session } from "./auth";
-import { Matcher } from "./filters";
+import { Matcher } from "./helpers/filters";
+import { ArrayHandler } from "./helpers/handlers";
 import { metadata } from "./components";
+import { MetadataProtocol } from "./components/interfaces/metadata_protocol";
 
 const session = new Session();
 
-const components = Object.values(metadata);
+const metadataArray = Object.values(metadata);
+const components = new ArrayHandler(metadataArray);
 
-session.on("message_create", (msg) => {
+session.on("message_create", async (msg) => {
   const matcher = new Matcher(msg);
 
-  components
-    .filter((c) => c.trigger == "message_create")
-    .filter((c) => matcher.alias(c.alias))
-    .forEach((c) => c.template(msg));
+  const results: Array<MetadataProtocol> = await components
+    .filter(async (c) => c.trigger == "message_create")
+    .filter(async (c) => matcher.alias(c.alias))
+    .filter(async (c) => matcher.scope(c.scope))
+    .resolve();
+
+  results.forEach((c) => c.template(msg));
 });
 
 session.start();
