b61e7b1713476c811493a6634258d2660f9a3fd0
BearingMe <gomes.bruno201@gmail.com>
2021-10-16 01:55:56 -0300

    [FEAT] adicionar cargo a interface do meta
    a interface dos metadados agora aceitam tipos de cargos

--- src/components/interfaces/metadata_protocol.ts
+++ src/components/interfaces/metadata_protocol.ts
@@ -1,9 +1,11 @@
-type TriggerTypes = "message" | "message_create"
-type ScopeTypes = "anywhere" | "group_only" | "private_only"
+type TriggerTypes = "message" | "message_create";
+type ScopeTypes = "anywhere" | "group_only" | "private_only";
+type RoleTypes = "anyone" | "creator_only" | "staff_only";
 
 export interface MetadataProtocol {
   alias: RegExp;
   trigger: TriggerTypes;
-  scope?: ScopeTypes,
-  template(msg: any): void
+  scope?: ScopeTypes;
+  role?: RoleTypes;
+  template(msg: any): void;
 }
