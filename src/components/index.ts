3b13c0a717ec3c140a481f12ba04791136ba7db1
BearingMe <gomes.bruno201@gmail.com>
2021-10-16 03:45:31 -0300

    [FEAT] atualiza com as novas funções
    os comentários na lateral são para documentação

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,27 @@
 import { about } from "./_about";
+import { bot } from "./_bot";
+import { commands } from "./_commands";
 import { count } from "./_count";
 import { creator } from "./_creator";
+import { docs } from "./_docs";
 import { flip } from "./_flip";
 import { forbidden_link } from "./_forbidden_link";
 import { links } from "./_links";
 import { jokenpo } from "./_jokenpo";
+import { promote } from "./_promote";
+import { remove } from "./_remove";
 
 export const metadata = {
-  about,
-  count,
-  creator,
-  flip,
-  forbidden_link,
-  links,
-  jokenpo,
+  about, // salvo
+  bot, // salvo
+  commands, // não é necessário
+  count, // salvo
+  creator, // salvo
+  docs, // ainda não está pronta
+  flip, // salvo
+  forbidden_link, // não é necessário
+  jokenpo, // salvo
+  links, // salvo
+  promote, // salvo
+  remove, // salvo
 };
