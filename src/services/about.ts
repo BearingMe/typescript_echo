5f80da7203ec7a6f7ce784b2863b08759957625c
bruno gomes <gomes.bruno201@gmail.com>
2023-01-24 13:57:00 -0300

    feat: create about setup

---
+++ src/services/about.ts
@@ -0,0 +1,10 @@
+export const setup = {
+  name: "about",
+  alias: /^!about$/,
+  version: "0.1.0",
+  description: "About this bot",
+  long_description: "",
+  author: "Smith",
+};
+
+export const template = () => {};
